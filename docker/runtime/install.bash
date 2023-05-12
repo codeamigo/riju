af663a30eb42011574dc3af204239f8e911a9859
plondon <philip.n.london@gmail.com>
2023-05-12 09:21:35 -0400

    watchexec pin v

--- docker/runtime/install.bash
+++ docker/runtime/install.bash
@@ -72,8 +72,7 @@ apt-get install -y $(sed 's/#.*//' <<< "${packages}")
 
 pip3 install poetry --break-system-packages
 
-ver="$(latest_release watchexec/watchexec | sed 's/^cli-v//')"
-wget "https://github.com/watchexec/watchexec/releases/download/cli-v${ver}/watchexec-${ver}-x86_64-unknown-linux-gnu.deb"
+wget "https://github.com/watchexec/watchexec/releases/download/cli-v${1.22.1}/watchexec-${1.22.1}-x86_64-unknown-linux-gnu.deb"
 apt-get install -y ./watchexec-*.deb
 
 rm -rf /var/lib/apt/lists/*
